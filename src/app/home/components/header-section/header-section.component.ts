5a1e799f526a34db3f007866cdb96c82b235cc8b
Juansa García <dedalus1977@gmail.com>
2024-01-31 20:18:15 +0100

    header changes, degree and history component fixes

--- src/app/home/components/header-section/header-section.component.ts
+++ src/app/home/components/header-section/header-section.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header-section',
   standalone: true,
   imports: [],
   templateUrl: './header-section.component.html',
-  styleUrl: './header-section.component.css'
+  styleUrl: './header-section.component.css',
 })
 export class HeaderSectionComponent {
+   textos:string[] = [
+    'escritora',
+    'vegetariana',
+    'cuidadora de gatos',
+    'adicta a la comida'
+   ]
+textToShow:string=""
 
+constructor(){
+  this.textChange()
+
+}
+
+
+  textChange() {
+    let index = 0;
+    setInterval(()=>{
+      this.textToShow = this.textos[index];
+      index = (index + 1) % this.textos.length;
+      console.log(this.textToShow)
+    }, 1560)
+
+  }
 }
